8225be3a7bbcd6843091ef7579e10b31c8121385
silenuszhi <silenuszhi@gmail.com>
2018-08-23 21:00:15 +0800

    Add package-list.txt to show andconda installed package.

--- run.sh
+++ run.sh
@@ -20,12 +20,36 @@
 
 #  Todo: fix anaconda dependent
 
+
+echo "This is a script help you download the caffe alexnet model and transfer it into tensorflow models"
+echo "Written by sileuszhi"
 #  0. clone the caffe-tensorflow repo.
+
 git clone https://github.com/silenuszhi/caffe-tensorflow
 cd ./caffe-tensorflow
 mkdir output
 mkdir download
-curl http://dl.caffe.berkeleyvision.org/bvlc_alexnet.caffemodel ./download/bvlc_alexnet.caffemodel
-curl https://raw.githubusercontent.com/BVLC/caffe/master/models/bvlc_alexnet/deploy.prototxt ./download/deploy.prototxt
+echo "Downloading Caffe AlexNet Model..."
+curl http://dl.caffe.berkeleyvision.org/bvlc_alexnet.caffemodel -o ./download/bvlc_alexnet.caffemodel --progress
+echo "Downloading Caffe AlexNet Deploy..."
+curl https://raw.githubusercontent.com/BVLC/caffe/master/models/bvlc_alexnet/deploy.prototxt -o ./download/deploy.prototxt --progress
 
-bash ./convert.py def_path ./download/bvlc_alexnet.caffemodel --caffemodel ./download/deploy.prototxt 
+# 1. convert the model
+~/anaconda2/envs/tf_alexnet/bin/python ./convert.py ./download/deploy.prototxt --caffemodel ./download/bvlc_alexnet.caffemodel --data-output-path ./output/bvlc_alexnet.npy
+
+# 2. Download GroundTruthTable of ILSVRC2012 Validation Set in Caffe Order
+# https://stackoverflow.com/questions/42537483/ilsvrc2012-validation-ground-truth-labels-index-not-matching-pretrained-networks
+# https://github.com/BVLC/caffe/blob/master/data/ilsvrc12/get_ilsvrc_aux.sh
+
+echo "This forks from https://github.com/BVLC/caffe/blob/master/data/ilsvrc12/get_ilsvrc_aux.sh"
+
+echo "Downloading..."
+
+curl http://dl.caffe.berkeleyvision.org/caffe_ilsvrc12.tar.gz -o ./output/caffe_ilsvrc12.tar.gz
+
+echo "Unzipping..."
+
+mkdir -p ./output/caffe_ilsvrc12 && tar -xf ./output/caffe_ilsvrc12.tar.gz -C  ./output/caffe_ilsvrc12 && rm -f ./output/caffe_ilsvrc12.tar.gz
+
+echo "Done."
+
